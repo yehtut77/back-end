55114553e1bfa700db32360c09d233ed853ebc8c
yehtut77 <yehtut77.tgi@gmail.com>
2024-03-02 14:28:15 +0900

    server

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const { report } = require('./modules/report');
 const jwt = require('jsonwebtoken'); // Make sure jsonwebtoken is imported
 
 const corsOptions = { 
-  origin: 'https://vercel.com/ye-htut-khaungs-projects/hs-cargo/EZCxEHommeLhERc4YPrNexMmNCcc', // Specify the origin you are allowing
+  origin: 'https://hs-cargo-kcio04wvk-ye-htut-khaungs-projects.vercel.app', // Specify the origin you are allowing
   credentials: true, // This is important for cookies, authorization headers with HTTPS
   optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
 };
