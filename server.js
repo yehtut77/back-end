e39b4d45ce8189d482beb3404457985a746133dc
yehtut77 <yehtut77.tgi@gmail.com>
2024-03-09 14:56:41 +0900

    allowed origin

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const limiter = rateLimit({
 });
 
 const allowedOrigins = [
-  'https://hs-cargo-kcio04wvk-ye-htut-khaungs-projects.vercel.app',
+  'https://hs-cargo-iqh3jnxex-ye-htut-khaungs-projects.vercel.app',
   'https://hs-cargo.vercel.app',
   
 ];
@@ -81,7 +81,10 @@ app.post('/login', (req, res, next) => {
     };
 
     const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '24h' });
-    res.cookie('token', token, { httpOnly: true, secure: process.env.NODE_ENV === 'production', sameSite: 'strict', maxAge: 24 * 60 * 60 * 1000 });
+    res.cookie('token', token, { httpOnly: true, 
+      secure: process.env.NODE_ENV === 'production', 
+      sameSite: 'strict', 
+      maxAge: 24 * 60 * 60 * 1000 });
 
     res.status(200).json({ message: 'ok', userId: user.user_id, isAdmin: user.isAdmin });
   })(req, res, next);
